ea76883770fddd30f3ca1ed96237d8c58f044d5d
Bal Krishna Poudel <baalkissna2012@gmail.com>
2020-03-18 00:06:30 +0100

    PE-101 A blank window is created!

--- README.md
+++ README.md
@@ -4,6 +4,10 @@ C++ Project that uses SDL2 library ( openGL and Direct3D)
 This project is based on the lectures from `cave of programming` :[YouTube video series](https://www.youtube.com/watch?v=1MKhigIml3E&list=PLmpc3xvYSk4wDCP5zjt2QQXe8-JGHa4Kt).
 This project provides some basic concepts about 3D gaming.
 
+## Prerequisites
+1. Install the SDL on your machine
+2. Set up the build system properly
+
 ## Using uncrustify with repo
 
 A python script needs to automate the process of uncrustify. The script should detect automatically all the source files that have been changed and should run the uncrustify to all those changed files.
--- main.cpp
+++ main.cpp
@@ -6,13 +6,41 @@ using namespace std;
 
 int main()
 {
+	const int SCREEN_WIDTH = 800;
+	const int SCREEN_HEIGHT = 800;
+
    /* Initialize the SDL */
    if ((SDL_INIT_VIDEO) < 0)
    {
       cout << "SDL init failed." << endl;
       return(1);
    }
-   cout << "SDL Init succeeded." << endl;
+  
+   SDL_Window *window = SDL_CreateWindow("Particle file explosion",
+		   SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+
+   if(window == NULL) {
+	   SDL_Quit();
+	   return 2;
+   }
+
+   bool quit = false;
+   SDL_Event event;
+
+   while(!quit){
+	   // update particles
+	   // Draw particles
+	   // Check for messages/events
+	   while(SDL_PollEvent(&event)){
+
+		   if(event.type == SDL_QUIT) {
+			   quit = true;
+		   }
+	   }
+   }
+
+
+   SDL_DestroyWindow(window);
    SDL_Quit();
 
    return(0);
