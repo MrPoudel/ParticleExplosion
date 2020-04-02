9659cfd69e723edce98a195727d65d31d5c076e2
Bal Krishna Poudel <baalkissna2012@gmail.com>
2020-04-02 18:42:02 +0200

    PE-101 uncrustified.

--- main.cpp
+++ main.cpp
@@ -3,7 +3,6 @@
 #include <SDL.h>
 #include "Screen.h"
 
-
 using namespace    std;
 using namespace    caveofprogramming;
 
@@ -12,20 +11,19 @@ int main()
 
   // 0xFF123456
 
-  unsigned char alpha = 0xFF;
-  unsigned char red = 0x12;
-  unsigned char green = 0x34;
-  unsigned char blue = 0x56;
-  
+  unsigned char    alpha = 0xFF;
+  unsigned char    red   = 0x12;
+  unsigned char    green = 0x34;
+  unsigned char    blue  = 0x56;
 
-  unsigned int color = 0;
-  color += alpha;
+  unsigned int    color = 0;
+  color  += alpha;
   color <<= 8;
-  color += red;
+  color  += red;
   color <<= 8;
-  color += green;
+  color  += green;
   color <<= 8;
-  color += blue;
+  color  += blue;
 
   cout << setfill('0') << setw(8) << hex << color << endl;
 
