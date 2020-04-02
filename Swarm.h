68b2beb79c4857c92f9b4c523a345329698f905d
Bal Krishna Poudel <baalkissna2012@gmail.com>
2020-04-03 01:28:14 +0200

    EN-101 added Swarm class.

---
+++ Swarm.h
@@ -0,0 +1,27 @@
+#ifndef SWARM_H_
+#define SWARM_H_
+
+#include "Particle.h"
+
+namespace    caveofprogramming {
+
+  class    Swarm {
+public:
+    const static int    NPARTICLES = 5000;
+
+private:
+    Particle*   m_pParticles;
+
+public:
+    Swarm();
+    virtual ~Swarm();
+
+    const Particle* const getParticles()
+    {
+      return m_pParticles;
+    }
+
+  };
+} /* namespace caveofprogramming */
+
+#endif /* SWARM_H_ */
