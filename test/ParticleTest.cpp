c3dd7c4299182193d234534508d5877de843f5c8
Bal Krishna Poudel <baalkissna2012@gmail.com>
2020-07-28 13:14:24 +0200

    Added dummy a test that verifies gtest setup.

---
+++ test/ParticleTest.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+#include "Particle.h"
+#include  <math.h>
+#include  <stdlib.h>
+
+#include <gtest/gtest.h>
+
+struct ParticleTest : public :: testing::Test {
+    virtual void SetUp() override {
+    
+        }
+
+    virtual void TearDown() override {
+    
+        }
+};
+
+TEST_F(ParticleTest, Dummytest){
+    EXPECT_EQ("hello", "hello");
+    EXPECT_EQ(5, 7);
+}
