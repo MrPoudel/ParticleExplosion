872c5b4297ce57c60d427fe7ee2d92093230595d
Bal Krishna Poudel <baalkissna2012@gmail.com>
2020-07-28 12:40:49 +0200

    Uncrustified main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
-#include <iostream>
 #include <iomanip>
-#include <SDL.h>
+#include <iostream>
 #include <math.h>
 #include <stdlib.h>
+#include <SDL.h>
 #include <time.h>
 
 #include "Screen.h"
@@ -11,65 +11,64 @@
 using namespace    std;
 using namespace    caveofprogramming;
 
+
 int main()
 {
-  int    color = 0x123456;
+   int           color = 0x123456;
 
-  int              red   = (color & 0xFF0000) >> 16;
-  int              green = (color & 0x00FF00) >> 8;
-  unsigned char    blue  = (color & 0x0000FF);
+   int           red   = (color & 0xFF0000) >> 16;
+   int           green = (color & 0x00FF00) >> 8;
+   unsigned char blue  = (color & 0x0000FF);
 
-  cout << hex << (int) blue << (int) red << endl;
+   cout << hex << (int)blue << (int)red << endl;
 
-  srand(time(NULL));
-  Screen    screen;
-  int       elapsed;
+   srand(time(NULL));
+   Screen screen;
+   int    elapsed;
 
-  if (screen.init() == false)
-  {
-    cout << "Error initializing SDL." << endl;
-  }
+   if (screen.init() == false)
+   {
+      cout << "Error initializing SDL." << endl;
+   }
+   Swarm                 swarm;
+   const Particle *const pParticles = swarm.getParticles();
 
-  Swarm                    swarm;
-  const Particle* const    pParticles = swarm.getParticles();
+   while (true)
+   {
+      // Update particles
+      // Draw particles
 
-  while (true)
-  {
-    // Update particles
-    // Draw particles
+      elapsed = SDL_GetTicks();
+      // screen.clear();
+      swarm.update(elapsed);
 
-    elapsed = SDL_GetTicks();
-    // screen.clear();
-    swarm.update(elapsed);
+      unsigned char red   = (1 + sin(elapsed * 0.001)) * 128;
+      unsigned char green = (1 + sin(elapsed * 0.002)) * 128;
+      unsigned char blue  = (1 + sin(elapsed * 0.003)) * 128;
 
-    unsigned char    red   = (1 + sin(elapsed * 0.001)) * 128;
-    unsigned char    green = (1 + sin(elapsed * 0.002)) * 128;
-    unsigned char    blue  = (1 + sin(elapsed * 0.003)) * 128;
+      for (int i = 0; i < Swarm::NPARTICLES; i++)
+      {
+         Particle particle = pParticles[i];
 
-    for (int i = 0; i < Swarm::NPARTICLES; i++)
-    {
-      Particle    particle = pParticles[i];
+         // int    x = (particle.m_x + 1) * Screen::SCREEN_WIDTH / 2;
+         // int    y = (particle.m_y + 1) * Screen::SCREEN_HEIGHT / 2;
 
-      // int    x = (particle.m_x + 1) * Screen::SCREEN_WIDTH / 2;
-      // int    y = (particle.m_y + 1) * Screen::SCREEN_HEIGHT / 2;
+         int x = (particle.m_x + 1) * Screen::SCREEN_WIDTH / 2;
+         int y = (particle.m_y * Screen::SCREEN_HEIGHT / 2) + Screen::SCREEN_HEIGHT / 2;
 
-      int    x = (particle.m_x + 1) * Screen::SCREEN_WIDTH / 2;
-      int    y = (particle.m_y * Screen::SCREEN_HEIGHT / 2) + Screen::SCREEN_HEIGHT / 2;
+         screen.setPixel(x, y, red, green, blue);
+      }
 
-      screen.setPixel(x, y, red, green, blue);
-    }
+      screen.boxBlur();
 
-    screen.boxBlur();
+      screen.update();
 
-    screen.update();
+      if (screen.processEvents() == false)
+      {
+         break;
+      }
+   }
+   screen.close();
 
-    if (screen.processEvents() == false)
-    {
-      break;
-    }
-  }
-
-  screen.close();
-
-  return 0;
+   return(0);
 } // main
