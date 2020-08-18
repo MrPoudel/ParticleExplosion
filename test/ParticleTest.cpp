a5765536417213b6a7207acee08305f873bc17fc
MrPoudel <baalkissna2012@gmail.com>
2020-08-18 23:20:54 +0200

    Minor changes

--- test/ParticleTest.cpp
+++ test/ParticleTest.cpp
@@ -16,7 +16,26 @@ struct ParticleTest : public :: testing::Test {
         }
 };
 
-TEST_F(ParticleTest, Dummytest){
-    EXPECT_EQ("hello", "hello");
-    EXPECT_EQ(7, 7);
+TEST_F(ParticleTest, constructParticle){
+    caveofprogramming::Particle p;   
+    
+
+    EXPECT_EQ(p.m_x, 0);
+    EXPECT_EQ(p.m_y, 0);
+
+    p.update(1000);
+
+    EXPECT_NEAR(p.m_x, 0.189675, 0.00001);
+    EXPECT_NEAR(p.m_y, -0.161105, 0.0001);
 }
+
+TEST_F(ParticleTest, Dummytest2){
+    EXPECT_EQ("hello", "hello");
+    EXPECT_EQ(9, 9);
+}
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
+
